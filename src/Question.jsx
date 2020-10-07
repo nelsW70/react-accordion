93912db28b9c5e04806ec219266ccbc056bb5a0e
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-07 10:13:20 -0400

    add: show/hide functionality and new icon library to avoid unresolved bug in previous library

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import { GrAdd, GrSubtract } from 'react-icons/gr';
+
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <GrSubtract /> : <GrAdd />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
